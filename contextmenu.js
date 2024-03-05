1e825b7c2d46684fc2d6e0b2fcd27a9d4f4b3467
libDarkstreet <tkristof@protonmail.ch>
2024-03-05 21:51:55 +0100

    login_required error fixed

--- contextmenu.js
+++ contextmenu.js
@@ -1,29 +1,14 @@
-// Get current tab URL
-function getCurrentTabUrl() {
-    return new Promise((resolve, reject) => {
-        chrome.tabs.query({
-                active: true,
-                lastFocusedWindow: true
-            },
-            tabs => {
-                var tab = tabs[0]
-                var tab_url = tab.url
-                resolve(tab_url)
-            }
-        )
-    })
-}
-// Create context menu
 chrome.contextMenus.create({
-    title: 'Download Profile Picture',
-    contexts: ['all'],
-    onclick: () => {
-        getCurrentTabUrl().then(url => {
-            // facebook
-            if (url.includes('instagram.com')) {
-                getIgAvt(url)
-            }
-        })
-    },
-    documentUrlPatterns: ['*://*.instagram.com/*']
+  title: 'Download Profile Picture',
+  contexts: ['all'],
+  onclick: () => {
+    getTabUrl().then(url => {
+      if (url.includes('instagram.com')) {
+        getIgUserProfilePicture(url)
+      } else {
+        alert('Unsupported Website')
+      }
+    })
+  },
+  documentUrlPatterns: ['https://*.instagram.com/*']
 })
--- instagram.js
+++ instagram.js
@@ -1,75 +1,74 @@
-const userAgent = "Mozilla/5.0 (Linux; Android 10.1.0; motorola one Build/OPKS28.63-18-3; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 Instagram 72.0.0.21.98 Android (27/8.1.0; 320dpi; 720x1362; motorola; motorola one; deen_sprout; qcom; pt_BR; 132081645)"
+function getIgUserProfilePicture(url) {
+  getIgUsername(url).then(getIgUserId).then(openIgUserPicture)
+}
+
+const userAgent = "Mozilla/5.0 (Linux; Android 13; ONEPLUS A6013 AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/112.0.5615.48 Mobile Safari/537.36 Instagram 317.0.0.34.109 Android (33/13; 450dpi; 1080x2126; OnePlus; ONEPLUS A6013; OnePlus6T; qcom; en_US; 563459856)"
+
+function getTabUrl() {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query(
+      {
+        active: true,
+        lastFocusedWindow: true
+      },
+      tabs => {
+        var tab = tabs[0]
+        var tab_url = tab.url
+        resolve(tab_url)
+      }
+    )
+  })
+}
 
 function getIgUsername(link) {
-    return new Promise((resolve, reject) => {
-        let regex = /(?<=instagram.com\/)[A-Za-z0-9_.]+/
-        let username = link.match(regex)[0]
-        resolve(username)
-    })
+  return new Promise((resolve, reject) => {
+      let regex = /(?<=instagram.com\/)[A-Za-z0-9_.]+/
+      let username = link.match(regex)[0]
+      resolve(username)
+  })
+}
+
+function getIgUserId(username) {
+  return new Promise((resolve, reject) => {
+    let url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${username}`
+    modifyHeaders(userAgent)
+
+    fetch(url)
+      .then(res => res.json())
+      .then(out => resolve(out.data.user.id))
+  })
 }
 
 function modifyHeaders(headerStr) {
-    chrome.webRequest.onBeforeSendHeaders.addListener(
-        function (details) {
-            for (var header of details.requestHeaders) {
-                if (header.name.toLowerCase() === 'user-agent') {
-                    header.value = headerStr
-                }
-            }
-            return {
-                requestHeaders: details.requestHeaders
-            }
-        }, {
-            urls: ['https://i.instagram.com/api/v1/users/*']
-        },
-        ['blocking', 'requestHeaders']
-    )
+  chrome.webRequest.onBeforeSendHeaders.addListener(
+      function (details) {
+          for (var header of details.requestHeaders) {
+              if (header.name.toLowerCase() === 'user-agent') {
+                  header.value = headerStr
+              }
+          }
+          return {
+              requestHeaders: details.requestHeaders
+          }
+      }, {
+          urls: ['https://i.instagram.com/api/v1/users/*']
+      },
+      ['blocking', 'requestHeaders']
+  )
 }
 
-function getIgUserID(username) {
-    return new Promise((resolve, reject) => {
-        let url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${username}`
-        modifyHeaders(userAgent)
+function openIgUserPicture(instagram_user_id) {
+  return new Promise((resolve, reject) => {
+    modifyHeaders(userAgent)
+    let url = `https://i.instagram.com/api/v1/users/${instagram_user_id}/info/`
 
-        fetch(url, {
-                'mode': "no-cors"
-            })
-            .then(res => res.json())
-            .then(out => resolve(out.data.user.id))
-    })
-}
-
-function getCookies(domain, name, callback) {
-    chrome.cookies.get({
-        "url": domain,
-        "name": name
-    }, function (cookie) {
-        if (callback) { callback(cookie.value); }
-    });
-}
-
-function openIgAvt(instagram_user_id) {
-    return new Promise((resolve, reject) => {
-        modifyHeaders(userAgent)
-        let url = `https://i.instagram.com/api/v1/users/${instagram_user_id}/info/`
-
-        getCookies("https://www.instagram.com", "sessionid", function (sessionid) {
-            fetch(url, {
-                    headers:{
-                        Cookie: "sessionid=" + sessionid
-                    }
-                })
-                .then(res => res.json())
-                .then(out => {
-                    console.log(out)
-                    let url = out.user.hd_profile_pic_url_info.url
-                    chrome.tabs.create({url})
-                    resolve(url)
-                })
-        });
-    })
-}
-
-function getIgAvt(url) {
-    getIgUsername(url).then(getIgUserID).then(openIgAvt)
+    fetch(url)
+      .then(res => res.json())
+      .then(out => {
+        console.log(out)
+        let url = out.user.hd_profile_pic_url_info.url
+        chrome.tabs.create({ url })
+        resolve(url)
+      })
+  })
 }
